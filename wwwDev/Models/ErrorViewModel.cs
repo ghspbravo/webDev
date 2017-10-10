c8ce4cc42aa6b096e438a7f3d23f953d7fbc35a1
ghspbravo <32223917+ghspbravo@users.noreply.github.com>
2017-10-10 12:20:20 +0500

    Добавьте файлы проекта.

---
+++ wwwDev/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace wwwDev.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
