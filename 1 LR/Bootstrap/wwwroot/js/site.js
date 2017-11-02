1ab3a427a919543f20b7e52baac6ea4960e37a74
ghspbravo <32223917+ghspbravo@users.noreply.github.com>
2017-11-02 16:57:33 +0500

    +LR1 (Bootstrap)

---
+++ 1 LR/Bootstrap/wwwroot/js/site.js
@@ -0,0 +1,20 @@
+﻿console.log('О, привет');
+
+$(document).ready(function(){
+	$('body').scrollspy({
+		target: '.sidebar',
+		offset: 200
+	});
+	$("#sidebar").affix({
+		offset: {
+			top: 250
+		}
+	});
+
+	$(document).scroll(function() {
+		var docheight = $('section').height() , delta = $('body').height()-docheight, winheight = $(window).height();
+		var wintop = $(window).scrollTop() - delta;
+		var totalScroll = (wintop/(docheight-winheight))*100;
+		$(".progress-bar").css("width",totalScroll+"%");
+	});
+});
