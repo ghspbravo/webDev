d2e30ab4372faa45537bec53821ce87f68139cd1
ghspbravo <32223917+ghspbravo@users.noreply.github.com>
2017-11-04 19:51:05 +0500

    + LR 2

---
+++ 2 LR/FormTask/Models/MessageModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FormTask.Models
+{
+    public class MessageModel
+    {
+        [Required(ErrorMessage = "Необходимо ввести адрес")]
+        [EmailAddress(ErrorMessage = "Неверный формат email")]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "Необходимо ввести тему")]
+        [StringLength(50, ErrorMessage = "Тема не должна превышать 50 символов")]
+        public string Topic { get; set; }
+
+        [Required(ErrorMessage = "Необходимо ввести сообщение")]
+        [StringLength(2000, ErrorMessage = "Сообщение слишком длинное. Попробуйте быть лаконичнее.")]
+        public string Text { get; set; }
+    }
+}
